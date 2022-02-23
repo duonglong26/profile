99d0235fa15f85f5551bace8423b745e276a8ad3
Duong Long <2051063530@e.tlu.edu.vn>
2022-02-23 23:45:16 +0700

    base body and add img

--- asset/js/HeaderComponent.js
+++ asset/js/HeaderComponent.js
@@ -2,6 +2,7 @@ handleMenu = (status) => {
     console.log(status)
     let menu = document.getElementById("header__bar-list");
     let iconBar = document.getElementById("header__bar-icon");
+    // debugger
     if (status === 'open') {
         menu.style.display = "block";
         iconBar.style.display = "none";
