77924bbe21d0c3aa057f0c5d5d96c77e0df46f34
Duong Long <2051063530@e.tlu.edu.vn>
2022-02-25 18:23:41 +0700

    home, header
    - add img
    - change background
    - fix scroll header

--- asset/js/HeaderComponent.js
+++ asset/js/HeaderComponent.js
@@ -1,8 +1,19 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        let header = document.getElementById("header");
+        if ($(this).scrollTop()) {
+            header.style.backgroundColor = "white";
+            header.style.transition = "all 0.2s ease"
+        } else {
+            header.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+        }
+    })
+})
+
 handleMenu = (status) => {
     console.log(status)
     let menu = document.getElementById("header__bar-list");
     let iconBar = document.getElementById("header__bar-icon");
-    // debugger
     if (status === 'open') {
         menu.style.display = "block";
         iconBar.style.display = "none";
@@ -11,3 +22,4 @@ handleMenu = (status) => {
         iconBar.style.display = "block";
     }
 }
+
