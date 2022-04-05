5a04ea2f32f455af11af7cc0e9a3a224d501ee5c
NguyenDinhThang1204 <2051063572@e.tlu.edu.vn>
2022-04-05 20:32:29 +0700

    navbar

--- react-frontend/src/app/views/Profile/Header/Header.jsx
+++ react-frontend/src/app/views/Profile/Header/Header.jsx
@@ -1,78 +1,101 @@
 import React, { memo } from "react";
-import styles from './_header.module.scss';
+import styles from "./_header.module.scss";
 import { Link } from "react-router-dom";
 import { ROOT_PATH } from "../../../../Const";
 import { AiOutlineBars } from "react-icons/ai";
-
+import { FaTimes } from "react-icons/fa";
 function Header() {
-
-    return (
-        <header className={styles.header} >
-            <nav className={styles.nav}>
-                <div className={styles.title}>
-                    <Link
-                        className={styles.link}
-                        to={ROOT_PATH + "/"}
-                    >
-                        My profile
-                    </Link>
-                </div>
-                <ul className={styles.listItem}>
-                    <li className={styles.item}>
-                        <a
-                            href="#home"
-                            className={styles.link}
-                        >
-                            Home
-                        </a>
-                    </li>
-                    <li className={styles.item}>
-                        <a
-                            href="#about-me"
-                            className={styles.link}
-                        >
-                            About me
-                        </a>
-                    </li>
-                    <li className={styles.item}>
-                        <a
-                            href="#education"
-                            className={styles.link}
-                        >
-                            Education
-                        </a>
-                    </li>
-                    <li className={styles.item}>
-                        <a
-                            href="#skills"
-                            className={styles.link}
-                        >
-                            Skills
-                        </a>
-                    </li>
-                    <li className={styles.item}>
-                        <a
-                            href="#experience"
-                            className={styles.link}
-                        >
-                            Experience
-                        </a>
-                    </li>
-                    <li className={styles.item}>
-                        <a
-                            href="#contact"
-                            className={styles.link}
-                        >
-                            Contact
-                        </a>
-                    </li>
-                
-                    <AiOutlineBars className="iconNav"/>
-                
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.title}>
+          <Link className={styles.link} to={ROOT_PATH + "/"}>
+            My profile
+          </Link>
+        </div>
+        <ul className={styles.listItem}>
+          <li className={styles.item}>
+            <a href="#home" className={styles.link}>
+              Home
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="#about-me" className={styles.link}>
+              About me
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="#education" className={styles.link}>
+              Education
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="#skills" className={styles.link}>
+              Skills
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="#experience" className={styles.link}>
+              Experience
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a href="#contact" className={styles.link}>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <label form={styles.nav_mb_input} className={styles.navbar}>
+            <AiOutlineBars/>
+        </label>
+        <input type="checkbox" className={styles.nav_mb_input} />
+        
+       
+        <div className={styles.nav_overlay}></div>
+        
+        <nav className={styles.nav_mb}>
+          <ul className={styles.listItem_mb}>
+              <div className={styles.times}>
+                <FaTimes/>
+              </div>
+            <li className={styles.item_mb}>
+              <a href="#home" className={styles.link_mb}>
+                Home
+              </a>
+            </li>
+            <li className={styles.item_mb}>
+              <a href="#about-me" className={styles.link_mb}>
+                About me
+              </a>
+            </li>
+            <li className={styles.item_mb}>
+              <a href="#education" className={styles.link_mb}>
+                Education
+              </a>
+            </li>
+            <li className={styles.item_mb}>
+              <a href="#skills" className={styles.link_mb}>
+                Skills
+              </a>
+            </li>
+            <li className={styles.item_mb}>
+              <a href="#experience" className={styles.link_mb}>
+                Experience
+              </a>
+            </li>
+            <li className={styles.item_mb}>
+              <a href="#contact" className={styles.link_mb}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      
+      
+      
+      </nav>
+    </header>
+  );
 }
 
-export default memo(Header)
+export default memo(Header);
