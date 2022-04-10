81afb8874b37db22f6eb79f6f6ee113df4df524c
Hienhandsome2002 <2051063835@e.tlu.edu.vn>
2022-04-10 23:23:30 +0700

    finish dropdown menu header
    finish dropdown menu header

--- react-frontend/src/app/views/Introduce/Header/Header.jsx
+++ react-frontend/src/app/views/Introduce/Header/Header.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 import { ROOT_PATH } from "../../../../Const";
 import { toast } from 'react-toastify';
 import { ThemeContext } from '../ProfileManagement/ProfileManagement';
-import { AiOutlineBars } from "react-icons/ai";
-import { FaTimes } from "react-icons/fa";
+
 
 toast.configure({
     autoClose: 3000,
@@ -61,49 +60,7 @@ function Header() {
                             </a>
                         }
                     </div>
-                    <label htmlFor={styles.nav_input_mb}>
-                        <AiOutlineBars className={styles.navbar} />
-                    </label>
-
-                   
-
-                    <nav className={styles.nav_mb}>
-                        <ul className={styles.listItem_mb}>
-                            <div>
-                                <FaTimes className={styles.exit} />
-                            </div>
-                            <li className={styles.item_mb}>
-                                <a href="#home" className={styles.link_mb}>
-                                    Home
-                                </a>
-                            </li>
-                            <li className={styles.item_mb}>
-                                <a href="#about-me" className={styles.link_mb}>
-                                    About me
-                                </a>
-                            </li>
-                            <li className={styles.item_mb}>
-                                <a href="#education" className={styles.link_mb}>
-                                    Education
-                                </a>
-                            </li>
-                            <li className={styles.item_mb}>
-                                <a href="#skills" className={styles.link_mb}>
-                                    Skills
-                                </a>
-                            </li>
-                            <li className={styles.item_mb}>
-                                <a href="#experience" className={styles.link_mb}>
-                                    Experience
-                                </a>
-                            </li>
-                            <li className={styles.item_mb}>
-                                <a href="#contact" className={styles.link_mb}>
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    
                 </nav>
             </header>
         </>
--- react-frontend/src/app/views/Profile/Header/Header.jsx
+++ react-frontend/src/app/views/Profile/Header/Header.jsx
@@ -1,10 +1,19 @@
-import React, { memo } from "react";
+import React, { memo, useState, useEffect } from "react";
 import styles from './_header.module.scss';
 import { Link } from "react-router-dom";
 import { ROOT_PATH } from "../../../../Const";
-import { AiOutlineBars } from "react-icons/ai";
+import clsx from "clsx";
+import {
+    CgMenuGridR,
+    CgChevronUp
+} from "react-icons/cg";
 
 function Header() {
+    const [isShowVerticalNav, setIsShowVerticalNav] = useState(false);
+
+    const handleShowVerticalNav = () => {
+        setIsShowVerticalNav(!isShowVerticalNav);
+    }
 
     return (
         <header className={styles.header} >
@@ -66,10 +75,72 @@ function Header() {
                             Contact
                         </a>
                     </li>
-                
-                    {/* <AiOutlineBars className="iconNav"/> */}
-                
                 </ul>
+
+                <CgMenuGridR className={styles.menu} onClick={() => handleShowVerticalNav()} />
+
+                {isShowVerticalNav &&
+                    <ul className={clsx(styles.listItemVertical)}>
+                        <li className={styles.item}>
+                            <a
+                                href="#home"
+                                className={styles.link}
+                                onClick={() => setIsShowVerticalNav(false)}
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a
+                                href="#about-me"
+                                className={styles.link}
+                                onClick={() => setIsShowVerticalNav(false)}
+                            >
+                                About me
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a
+                                href="#education"
+                                className={styles.link}
+                                onClick={() => setIsShowVerticalNav(false)}
+                            >
+                                Education
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a
+                                href="#skills"
+                                className={styles.link}
+                                onClick={() => setIsShowVerticalNav(false)}
+                            >
+                                Skills
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a
+                                href="#experience"
+                                className={styles.link}
+                                onClick={() => setIsShowVerticalNav(false)}
+                            >
+                                Experience
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a
+                                href="#contact"
+                                className={styles.link}
+                                onClick={() => setIsShowVerticalNav(false)}
+                            >
+                                Contact
+                            </a>
+                        </li>
+                        <CgChevronUp
+                            className={styles.hiddenNavVertical}
+                            onClick={() => setIsShowVerticalNav(false)}
+                        />
+                    </ul>
+                }
             </nav>
         </header>
     );
